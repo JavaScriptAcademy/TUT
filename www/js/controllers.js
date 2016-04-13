361ee50be993d9b2c5d2f3ede672add5a1d30dd4
Fiona Wang1 <Fiona_Wang1@epam.com>
2016-04-13 15:06:04 +0800

    init

---
+++ www/js/controllers.js
@@ -0,0 +1,26 @@
+angular.module('app.controllers', [])
+  
+.controller('createDefaultPageCtrl', function($scope) {
+
+})
+   
+.controller('listDefaultPageCtrl', function($scope) {
+
+})
+   
+.controller('meDefaultPageCtrl', function($scope) {
+
+})
+         
+.controller('signupCtrl', function($scope) {
+
+})
+   
+.controller('loginCtrl', function($scope) {
+
+})
+   
+.controller('liveCtrl', function($scope) {
+
+})
+ 
