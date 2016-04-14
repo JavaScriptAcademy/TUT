1edc20c73589be214c43d5d9a2d8026983c29e87
Phil-EPAM <Phil_Yang@epam.com>
2016-04-14 15:40:13 +0800

    revise the create page

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,7 +1,48 @@
-angular.module('app.controllers', [])
+angular.module('app.controllers', ['ionic'])
   
-.controller('createDefaultPageCtrl', function($scope) {
+.controller('createDefaultPageCtrl', function($scope, $ionicActionSheet, $timeout) {
+ // Triggered on a button click, or some other target
+ $scope.meeting = {
+ 	name:"",
+ 	host:"",
+ 	time:"",
+ 	description:"",
+ },
+ $scope.clear = function() {
+	 this.meeting = {
+	 	name:"",
+	 	host:"",
+	 	time:"",
+	 	description:"",
+	 }
+ }
+ $scope.show = function() {
+ 	console.log('111');
 
+   // Show the action sheet
+   var hideSheet = $ionicActionSheet.show({
+     buttons: [
+       { text: '<b>Share</b> This' },
+       { text: 'Move' }
+     ],
+     destructiveText: 'Delete',
+     titleText: 'Modify your album',
+     cancelText: 'Cancel',
+     cancel: function() {
+          // add cancel code..
+        },
+     destructiveButtonClicked: function(){
+     	console.log('delted');
+     },
+     buttonClicked: function(index) {
+       console.log(index);
+     }
+   });
+
+   // For example's sake, hide the sheet after two seconds
+// hideSheet()
+
+ };
 })
    
 .controller('listDefaultPageCtrl', function($scope) {
