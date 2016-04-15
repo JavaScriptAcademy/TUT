23611a22fbcbfad488d2527ac94e99fb7e4dce73
Fiona Wang1 <Fiona_Wang1@epam.com>
2016-04-15 10:18:54 +0800

    try to add comment and open camera but both meet some issue

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,26 +1,226 @@
-angular.module('app.controllers', [])
-  
+angular.module('app.controllers', ['app.services'])
+
 .controller('createDefaultPageCtrl', function($scope) {
 
 })
-   
+
 .controller('listDefaultPageCtrl', function($scope) {
 
 })
-   
+
 .controller('meDefaultPageCtrl', function($scope) {
 
 })
-         
+
 .controller('signupCtrl', function($scope) {
 
 })
-   
+
 .controller('loginCtrl', function($scope) {
 
 })
-   
-.controller('liveCtrl', function($scope) {
 
+.controller('liveCtrl', function($scope, $cordovaCamera, $stateParams, $state, $ionicPopup,$ionicLoading, $ionicPopup, TaskService) {
+  // $scope.settingavatar = {};
+   // document.addEventListener("deviceready", function openCamera () {
+  $scope.openCamera = function() {
+    var options = {
+          quality: 50,
+          destinationType: Camera.DestinationType.DATA_URL,
+          sourceType: Camera.PictureSourceType.CAMERA,
+          allowEdit: true,
+          encodingType: Camera.EncodingType.JPEG,
+          targetWidth: 100,
+          targetHeight: 100,
+          popoverOptions: CameraPopoverOptions,
+          saveToPhotoAlbum: false,
+         correctOrientation:true
+        };
+
+        $cordovaCamera.getPicture(options).then(function(imageData) {
+          // var image = document.getElementById('myImage');
+          $scope.imgURI = "data:image/jpeg;base64," + imageData;
+        }, function(err) {
+          // error
+        });
+  }
+
+  // $scope.task = {};
+  // $scope.defect = {};
+  $scope.allComments = {};
+
+  // $scope.avatar =  StoreService.GetVal("avatar");
+  // console.log("avatar: " + $scope.avatar);
+
+  // console.log("orgId and defectId:"+$stateParams.ttt);
+  //234234&35415
+
+  // var arr = $stateParams.ttt.split('&');
+  // var orgId = arr[0];
+  // var defectId = arr[1];
+  // StoreService.SetVal("pageID", 0);
+
+  // $ionicLoading.show({template:"Loading...",hideOnStateChange:true});
+
+    console.log("***********GetDefectDetai%%%%%%%%%%%%%%%%%l");
+
+
+  TaskService.GetDefectComment(function(response){
+
+    console.log("%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%");
+
+      console.log("size: " + response.data.data.length);
+      if(response.data.data.length > 0){
+        console.log("true");
+        $scope.haveComments = true;
+        $scope.allComments = response.data.data;
+        $scope.commentList = $scope.allComments.slice(0, 5);
+        if(response.data.data.length > 5){
+          $scope.haveMoreComments = true;
+        }else{
+          $scope.haveMoreComments = false;
+        }
+      }else{
+        $scope.haveComments = false;
+      }
+    });
+
+    $scope.addComment = function(){
+
+        $scope.editable = !$scope.editable;
+    }
+
+    $scope.submitComment = function(){
+      var defectComment = document.getElementById("defectComment").value;
+      TaskService.SubmitDefectComment(Comment, function(response){
+
+      clearDefaultText(defectComment);
+
+
+      function clearDefaultText (message)
+      {
+        var obj = document.getElementById("defectComment");
+
+        if(obj.value == message)
+        {
+          obj.value = "";
+        }
+
+        obj.onblur = function()
+        {
+          if(obj.value == "")
+          {
+             obj.value = message;
+          }
+        }
+      }
+
+      TaskService.GetDefectComment(function(response){
+        // $ionicLoading.hide();
+        console.log("size: " + response.data.data.length);
+        if(response.data.data.length > 0){
+          console.log("true");
+          $scope.haveComments = true;
+          $scope.allComments = response.data.data;
+          $scope.commentList = $scope.allComments.slice(0, 5);
+          if(response.data.data.length > 5){
+            $scope.haveMoreComments = true;
+          }else{
+            $scope.haveMoreComments = false;
+          }
+        }else{
+          $scope.haveComments = false;
+        }
+      });
+      });
+      $scope.editable = false;
+    }
 })
- 
+
+// .controller('liveCtrl', function($scope, $cordovaCamera, $cordovaFile) {
+//   $scope.settingavatar = {};
+//   $scope.openCamera = function() {
+//   console.log("add image");
+//   // console.log("cordova.file.dataDirectory:"+cordova.file.dataDirectory);
+//   // 2
+//   var options = {
+//   destinationType : Camera.DestinationType.FILE_URI,
+//   sourceType : Camera.PictureSourceType.CAMERA, // Camera.PictureSourceType.PHOTOLIBRARY
+//   allowEdit : true,
+//   encodingType: Camera.EncodingType.JPEG,
+//   popoverOptions: CameraPopoverOptions,
+//   };
+//   //               quality: 50,
+//   //               destinationType: Camera.DestinationType.DATA_URL,
+//   //               targetWidth: 100,
+//   //               targetHeight: 100,
+//   //               saveToPhotoAlbum: false
+//   // 3
+//   $cordovaCamera.getPicture(options).then(function(imageData) {
+//   // 4
+//   onImageSuccess(imageData);
+
+//   function onImageSuccess(fileURI) {
+//     createFileEntry(fileURI);
+//   }
+
+//   function createFileEntry(fileURI) {
+//     window.resolveLocalFileSystemURL(fileURI, copyFile, fail);
+//   }
+//   //5
+//   function copyFile(fileEntry) {
+//     var name = fileEntry.fullPath.substr(fileEntry.fullPath.lastIndexOf('/') + 1);
+//     var newName = makeid() + name;
+
+//     window.resolveLocalFileSystemURL(cordova.file.dataDirectory, function(fileSystem2) {
+//       fileEntry.copyTo(
+//       fileSystem2,
+//       newName,
+//       onCopySuccess,
+//       fail
+//       );
+//     },
+//     fail);
+//   }
+//   // 6
+//   function onCopySuccess(entry) {
+//   //   $scope.$apply(function () {
+//   //   // $scope.images.set(entry.nativeURL);
+//   //   $scope.images.push(entry.nativeURL);
+//   //   $scope.settingavatar.image=entry.nativeURL;
+//   //   $scope.images.cleanup();
+
+//   // });
+//   //add by Fiona 11_16
+//   // navigator.camera.cleanup( cameraSuccess, cameraError );
+
+//   // $scope.settingavatar.image=entry.nativeURL;
+//   }
+
+//   function fail(error) {
+//     console.log("fail: " + error.code);
+//   }
+
+//   function makeid() {
+//     var text = "";
+//     var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+//     for (var i=0; i < 5; i++) {
+//       text += possible.charAt(Math.floor(Math.random() * possible.length));
+//     }
+
+//     return text;
+//     }
+//     }, function(err) {
+//     console.log(err);
+//     });
+//   }
+
+//   $scope.urlForImage = function(imageName) {
+//     console.log("get correct path for image");
+//     var name = imageName.substr(imageName.lastIndexOf('/') + 1);
+//     var trueOrigin = cordova.file.dataDirectory + name;
+//     return trueOrigin;
+//   };
+
+// });
--- www/js/services.js
+++ www/js/services.js
@@ -1,5 +1,75 @@
 angular.module('app.services', [])
 
+.service('TaskService', function($http){
+
+// get defect comment:
+// http://localhost/project/rest/user/getDefectComment?defectId=16413&orgId=1492
+
+     function GetDefectComment( successCB, errorCB) {
+      // var taskCommentsUrl = ConfigService.GetTaskCommentsUrl+"?"+"taskID="+StoreService.GetVal("taskID") + "&pageID="+StoreService.GetVal("pageID");
+      var defectCommentUrl = "http://139.196.108.49/project/rest"+"?"+"defectId=&orgId=";
+
+      $http.get(defectCommentUrl)
+      .then(function successCallback(response) {
+        if (successCB) {
+
+          successCB(response);
+        };
+      }, function errorCallback(response) {
+        var tempData = {};
+        // console.log("page: " + StoreService.GetVal("pageID") % 2);
+
+        tempData = {"data":{"data":[
+             {"commentID": "1002","content":"this is comment 1",
+             "userName": "Jack", "avatarUrl":"http://7xn1dt.com1.z0.glb.clouddn.com/a.jpeg" , "attachment":"http://urltoattachment"},
+             {"commentID": "1002","content":"this is comment 2",
+             "userName": "Jack", "avatarUrl":"http://7xn1dt.com1.z0.glb.clouddn.com/a.jpeg" , "attachment":"http://urltoattachment"},
+             {"commentID": "1002","content":"this is comment 3",
+             "userName": "Jack", "avatarUrl":"http://7xn1dt.com1.z0.glb.clouddn.com/a.jpeg" , "attachment":"http://urltoattachment"},
+             {"commentID": "1002","content":"this is comment 4",
+             "userName": "Jack", "avatarUrl":"http://7xn1dt.com1.z0.glb.clouddn.com/a.jpeg" , "attachment":"http://urltoattachment"},
+             {"commentID": "1002","content":"this is comment 5",
+             "userName": "Jack", "avatarUrl":"http://7xn1dt.com1.z0.glb.clouddn.com/a.jpeg" , "attachment":"http://urltoattachment"},
+           ]}};
+        response = tempData;
+
+        console.log("GetDefectComments: error" + JSON.stringify(response));
+
+
+        if (errorCB) {
+
+          errorCB(response);
+        };
+      });
+      // var response = {"errorCode":"0","message":"Get Task List OK","data":[{"taskName":"test defect 2","dueDate":"2015-09-29 00:00:00.0"},{"taskName":"test defect 1","dueDate":"2015-08-14 00:00:00.0"},{"taskName":"task 1","dueDate":"2015-09-05 00:00:00.0"}]};
+      // return response.data;
+    }
+
+    // add defect comment:
+    // http://localhost/project/rest/user/addDefectComment?defectId=16413&orgId=1492&comment=skdjfsklf&userLoginName=chris.shu
+
+
+
+    // "SubmitDefectComment": function(defectId, orgId, defectComment, successCB, errorCB) {
+    //   var submitDefectCommentUrl = ConfigService.SubmitDefectCommnetUrl+"?"+"defectId="+defectId+"&orgId="+orgId+"&comment="+defectComment+"&userLoginName="+StoreService.GetVal("userName");
+
+    //   $http.get(submitDefectCommentUrl)
+    //   .then(function successCallback(response) {
+
+    //     if (successCB) {
+    //       successCB(response);
+    //     };
+    //   }, function errorCallback(response) {
+    //     if (errorCB) {
+    //       errorCB(response);
+    //     };
+    //   });
+    //   // var response = {"errorCode":"0","message":"Get Task List OK","data":[{"taskName":"test defect 2","dueDate":"2015-09-29 00:00:00.0"},{"taskName":"test defect 1","dueDate":"2015-08-14 00:00:00.0"},{"taskName":"task 1","dueDate":"2015-09-05 00:00:00.0"}]};
+    //   // return response.data;
+    // }
+
+})
+
 .factory('BlankFactory', [function(){
 
 }])
